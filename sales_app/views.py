d7ac825bffdf7c999a6329ae18d4d3b582ef8acf
ZEDDTAY8 <zeddtay123@gmail.com>
2025-10-08 11:27:56 +0500

    Пара заканчивается, сохранение последних изменений

--- sales_app/views.py
+++ sales_app/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+import os 
+import uuid 
+import xml.etree.ElementTRee as ET
+from django.shortcuts import render, redirect
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+from .forms import SaleForm
+
+def index(request):
+    form = SaleForm()
+    xml_files = []
+    xml_dir = os.path.join(settings.MEDIA_ROOT, 'xml_files')
+    if os.path.exist(xml_dir):
+        xml_files = [f for f in os.listdir(xml_dir) if f.endswith('.xml')]
+
+    xml_content = []
+    for file in xml_files:
+        try:
+            tree = ET.parse(os.path(xml_dir, file))
+            root = tree.hetroot()
+            sales = []
+            for sale in root.findall('sale'):
+                data = {
+                    'id': sale.get('id'),
+                    'product' sale.find('product').text if sale.find
+                    'price' sale.find
+                    'quantity' sale.find
+                    'date' sale.find
+                }
 
 # Create your views here.
