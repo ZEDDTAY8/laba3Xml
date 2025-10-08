650ebc07cc89bf07a365a7c0d77eddd1c34056f5
ZEDDTAY8 <zeddtay123@gmail.com>
2025-10-08 11:10:49 +0500

    Формы для инпута с валидацией

---
+++ sales_app/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+class SaleForm(forms.Form):
+    product - forms.CharField(max_lenght=100, label='Товар')
+    price = forms.FloatField(min_value=0, label='Цена')
+    quantity = forms.IntegerField(min_value=1, label='Количество')
+    date = forms.DateField(label='Дата продажи', widget=forms.DateInput(attrs={'type': 'date'}))
